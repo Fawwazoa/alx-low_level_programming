6be16b48f4248231ff581a593aef8eef83cb4adb
fawaz <fawazyousifabkr22.com>
2023-09-12 15:42:10 -0400

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,2 +1,13 @@
+#include "main.h"
 
+/**
+ * print_times_table - prints the times table
+ * @n: integer for which the times table will be printed
+ *
+ * Return: Always 0 (Success)
+ */
 
+void print_times_table(int n)
+{
+	retutn n;
+}	
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,13 @@
 /**
   * _islower - Entry point
   * Description: Checks for lowercase character.
+  * @c: Contains value to be compared
   * Return: Always 0 (Success)
   */
 
 int _islower(int c)
 {
-
+	/* ASCII value of lower case letters is between 97 & 122 */
 	if (c >=  97 && c <= 122)
 	{
 		return (1);
