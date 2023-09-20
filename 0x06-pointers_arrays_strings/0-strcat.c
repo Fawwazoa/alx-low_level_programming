548a6195c395ed6813fcc5396f069f9ab3feed13
fawaz <fawazyousifabkr22.com>
2023-09-20 11:52:49 -0400

    updated

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+		i++;
+	}
+	y = 0;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		i++;
+		j++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
