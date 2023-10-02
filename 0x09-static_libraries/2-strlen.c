52a318d58d26db016b464e59a61fab2f5b2a17c8
fawaz <fawazyousifabkr22.com>
2023-10-02 16:20:13 -0400

    updated

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: a character in a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
