e642db02569dd6457a0dde857029e858d1419019
fawaz <fawazyousifabkr22.com>
2023-10-04 16:17:40 -0400

    updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,45 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter
- *
- * @str: string given as a parameter
- * Return: pointer to a new string dublicate to str
- * if str == NULL, return NULL
+ * _strdup - Entry point
+ *@str: string we need to duplicate
+ * Return: a pointer to the duplicated string or NULL
  */
 char *_strdup(char *str)
 {
-	if (str != NULL)
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
+
+	if (str == NULL)
+		return (NULL);
+	for (n = 0; str[n] != '\0'; n++)
+		;
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+	if (strnew != NULL)
 	{
-		char *arr;
-		unsigned int i;
-		unsigned int size;
-
-		size = 0;
-		arr = (char *)malloc(sizeof(char) * (size + 1));
-
-		while (str[size] != '\0')
-		{
-			size++;
-		}
-
-		if (arr == NULL)
-		{
-			/* CHECK IF MALLOC SUCCEEDED */
-			fprintf(stderr, "Memory allocation failed\n");
-			return (NULL);
-		}
-
-		for (i = 0; i < size; i++)
-		{
-			*arr = *str;
-		}
-		arr[size + 1] = '\0';
-
-		return (str);
-	}
-	else
+		for (i = 0; str[i] != '\0'; i++)
+			strnew[i] = str[i];
+	} else
 	{
 		return (NULL);
 	}
+	strnew[i] = '\0';
+	return (strnew);
 }
